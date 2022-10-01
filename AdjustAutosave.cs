af3bf38b61842f62e3e36acc5381324633225c66
Stuco <15875181+stucoboots@users.noreply.github.com>
2022-10-01 15:35:35 -0500

    v1

---
+++ AdjustAutosave.cs
@@ -0,0 +1,70 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using BepInEx.Configuration;
+using Timberborn.Autosaving;
+
+namespace TimberbornAdjustAutosave
+{
+    [BepInPlugin("net.buhichan.stuco.plugins.timberbornadjustautosave", "Adjust Autosave", "1.0.0")]
+    [BepInProcess("Timberborn.exe")]
+    public class TimberbornAdjustAutosave : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("net.buhichan.stuco.plugins.timberbornadjustautosave");
+        private static string consoleOutPrefix = "[Adjust Autosave] ";
+
+        private static ConfigEntry<int> autosaveCountConfig;
+        private static int autosaveCountLowerBound = 1;
+        private static int autosaveCountUpperBound = 255;
+
+        private static ConfigEntry<float> autosaveFrequencyConfig; // game internally stores this as float but appears to round in use
+        private static float autosaveFrequencyLowerBound = 1f; // 1 minute
+        private static float autosaveFrequencyUpperBound = 1440f; // 24 hours
+
+        void Awake()
+        {
+            autosaveCountConfig = Config.Bind<int>("Autosave Count", "autosaveCount", 3, "Number of autosaves to keep per settlement. Must be between 1 and 255.");
+            autosaveFrequencyConfig = Config.Bind<float>("Autosave Frequency", "autosaveFrequency", 10f, "Autosave frequency (in minutes). Must be between 1 and 1440.");
+            harmony.PatchAll();
+        }
+
+        void OnDestroy()
+        {
+            harmony.UnpatchSelf();
+        }
+
+        private static bool isInRangeInclusive(float candidate, float lowerBound, float upperBound)
+        {
+            return lowerBound <= candidate && candidate <= upperBound;
+        }
+
+        [HarmonyPatch(typeof(Autosaver), "Awake")]
+        class Autosaver_Awake_Patch
+        {
+            static void Postfix(ref float ____frequencyInMinutes, ref int ____autosavesPerSettlement)
+            {
+                // autosave count
+                if (isInRangeInclusive(autosaveCountConfig.Value, autosaveCountLowerBound, autosaveCountUpperBound))
+                {
+                    ____autosavesPerSettlement = autosaveCountConfig.Value;
+                }
+                else
+                {
+                    Debug.LogWarning($"{consoleOutPrefix}Autosave count not modified. Verify that your config file value is between {autosaveCountLowerBound} and {autosaveCountUpperBound} inclusive.");
+                }
+                Debug.Log($"{consoleOutPrefix}Using autosave count of {____autosavesPerSettlement}.");
+
+                // autosave frequency
+                if (isInRangeInclusive(autosaveFrequencyConfig.Value, autosaveFrequencyLowerBound, autosaveFrequencyUpperBound))
+                {
+                    ____frequencyInMinutes = autosaveFrequencyConfig.Value;
+                }
+                else
+                {
+                    Debug.LogWarning($"{consoleOutPrefix}Autosave interval not modified. Verify that your config file value is between {autosaveFrequencyLowerBound} and {autosaveFrequencyUpperBound} inclusive.");
+                }
+                Debug.Log($"{consoleOutPrefix}Using autosave interval of {____frequencyInMinutes} minutes.");
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,15 @@
+# AdjustAutosave
+Adjust autosave frequency and number of autosaves kept.
+
+|Config Setting |Allowed Values|    Current Game Default|
+|---|---|---|
+|autosaveCount   |number between 1 and 255| 3|
+|autosaveFrequency[^1]   |number between 1 and 1440| 10|
+
+[^1]: This setting's unit is minutes.
+
+### **Setup Instructions**
+1. Ensure this mod and its dependencies are installed.
+2. Launch the game once. This generates the mod's config file. Located at `(your game install directory)/BepInEx/config/net.buhichan.stuco.plugins.timberbornadjustautosave.cfg`
+3. Change the values in the config file to your preference.
+4. Relaunch the game. Your settings will be applied from the config file.
